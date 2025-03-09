3525de92ef974c29f1b8c658b7d45df6a7742d41
M-14-deep <72016@aikogakuen.net>
2025-03-09 17:28:04 +0900

    Update scripts.js

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,7 +1,20 @@
-function run(){
-    var Url = window.location.href;
-  if (Url.length > 7){
-    var w = window.open("about:blank", "_blank", "status=no,toolbar=no,location=no,directories=no,resizable=yes,scrollbars=yes,width=9999,height=9999");
-    w.document.write(`<iframe style="height:100%; width: 100%; border: none; position: fixed; top: 0; right: 0; left: 0; bottom: 0; border: none;" sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-popups-to-escape-sandbox allow-presentation allow-same-origin allow-scripts allow-top-navigation allow-top-navigation-by-user-activation" src="${Url}"></iframe>`);
+const message1 = document.getElementById('message-1');
+
+const messages = [
+  '読み込み中',
+  '読み込み中.',
+  '読み込み中..',
+  '読み込み中...'
+];
+
+let i = 0;
+
+function showMessage_1() {
+  if (i == 4) {
+    i = 0;
   }
+  message1.textContent = messages[i];
+  i++;
 }
+
+setInterval(showMessage_1, 2000);
