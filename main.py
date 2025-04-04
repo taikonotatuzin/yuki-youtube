538af39c10a9eccd64b86d64ab684fdcda41195c
M-14-deep <72016@aikogakuen.net>
2025-04-04 18:25:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -717,7 +717,15 @@ def list_page(response: Response, request: Request):
 @app.get("/chat", response_class=HTMLResponse)
 def list_page(response: Response, request: Request):
     return template("chat.html", {"request": request})
-
+@app.get("/drive", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template(drive.html", {"request": request})
+@app.get("/paper", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("paper.html", {"request": request})
+@app.get("/snow", response_class=HTMLResponse)
+def list_page(response: Response, request: Request):
+    return template("snow.html", {"request": request})
 
 
 
